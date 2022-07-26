4e4d8b97552b2c2a88dfc04698e91004463a085c
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2022-07-26 17:35:08 -0300

    chore: core-23 init header but tailwindcss not working

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div :class="$style.nav">Navbar</div>
+</template>
+
+<script>
+export default {
+  name: "TheHeader",
+};
+</script>
+
+<style lang="postcss" module>
+.nav {
+  @apply text-green-800;
+}
+</style>
