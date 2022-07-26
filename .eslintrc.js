247c4be6a410033ebb5edc95716b985879271d2b
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2022-07-25 23:19:36 -0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: {
+      js: "espree",
+      ts: "@typescript-eslint/parser",
+      "<template>": "espree",
+    },
+  },
+  plugins: ["@typescript-eslint"],
+  env: {
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/vue3-recommended",
+    "prettier",
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  },
+};
---
+++ src/site.ts
@@ -0,0 +1,4 @@
+export default {
+  title: "Osmar Júnior Advogado",
+  description: "Osmar Júnior, especialista em casos criminais e família",
+};
