13ff8eef015fe8f0c55886ef0ed4113b473cf9f8
Lucas <lucas.ribeiro711@gmail.com>
2022-08-09 15:29:46 -0300

    fix: customers component width

--- src/components/TheCustomers.vue
+++ src/components/TheCustomers.vue
@@ -54,11 +54,11 @@
   @apply text-center text-3xl font-extrabold text-gray-900 sm:text-4xl;
 }
 .container {
-  @apply flex gap-4 overflow-x-scroll snap-mandatory max-w-xs lg:max-w-full;
+  @apply flex overflow-x-scroll snap-mandatory max-w-[22.5rem] lg:max-w-full;
 }
 
 .card {
-  @apply flex flex-col justify-center items-center text-center bg-white rounded-md p-4 shadow-md min-w-[224px] w-56 h-72;
+  @apply flex flex-col justify-center items-center text-center bg-white mx-4 rounded-md p-4 shadow-md min-w-[224px] w-56 h-72;
 }
 
 span {
